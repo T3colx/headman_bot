ecfa4cb63c9cda417cee5cc1273c5302c69bf0ac
Dmitry Efimov <t3colx@gmail.com>
2020-09-15 23:28:20 +0300

    Release version 1.0!

--- venv/Scripts/easy_install-3.8-script.py
+++ venv/Scripts/easy_install-3.8-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\t3col\PycharmProjects\HeadMan_bot_git\venvb\Scripts\python.exe
+#!C:\Users\t3col\PycharmProjects\HeadMan_bot_git\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.8'
 __requires__ = 'setuptools==40.8.0'
 import re
